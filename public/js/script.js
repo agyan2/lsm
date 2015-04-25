e1e1f9c75193904da275b803d4ef6daea1421755
agyan2 <abhinav0508@gmail.com>
2015-04-25 02:15:52 -0700

    second commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,47 +1,91 @@
-	// create the module and name it scotchApp
-	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+	// create the module and name it lsmApp
+	var lsmApp = angular.module('lsmApp', ['ngRoute']);
 
 	// configure our routes
-	scotchApp.config(function($routeProvider) {
+	lsmApp.config(function($routeProvider) {
 		$routeProvider
 
 			// route for the home page
 			.when('/', {
+				templateUrl : 'pages/home.html',
+				controller  : 'homeController'
+			})
+
+			// route for the brand page
+			.when('/brand', {
 				templateUrl : 'pages/brand.html',
 				controller  : 'brandController'
 			})
 
-			// route for the about page
+			// route for the reward page
 			.when('/reward', {
 				templateUrl : 'pages/reward.html',
 				controller  : 'rewardController'
 			})
 
 
-			// route for the campaign page
+			// route for the catalog page
 			.when('/catalog', {
 				templateUrl : 'pages/catalog.html',
 				controller  : 'catalogController'
 			})
 
-			// route for the contact page
-			.when('/catalog', {
-				templateUrl : 'pages/catalog.html',
-				controller  : 'catalogController'
+			// route for the campaign page
+			.when('/campaign', {
+				templateUrl : 'pages/campaign.html',
+				controller  : 'campaignController'
 			});
-	});
+			
+		});
+
 
 	// create the controller and inject Angular's $scope
-	scotchApp.controller('brandController', function($scope) {
+	lsmApp.controller('homeController', function($scope) {
 		// create a message to display in our view
-		$scope.message = 'Brand come and see how good I look!';
+		$scope.message = 'Welcome to LSM!';
+
 	});
 
-	scotchApp.controller('rewardController', function($scope) {
+
+	// create the controller and inject Angular's $scope
+	lsmApp.controller('brandController', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'Add or Edit Brands!!!!';
+
+		
+		$scope.createBrand = function(){
+
+			$scope.message=$scope.brandName;
+		};
+	});
+
+	lsmApp.controller('rewardController', function($scope) {
 		$scope.message = 'Reward! I am an about page.';
+
+		$scope.createReward = function(){
+
+			$scope.message=$scope.rewardName;
+		};
+
 	});
 
-	scotchApp.controller('catalogController', function($scope) {
+	lsmApp.controller('catalogController', function($scope) {
 		$scope.message = 'catalog us! JK. This is just a demo.';
+	
+		$scope.createCatalog = function(){
+
+			$scope.message=$scope.catalogName;
+		};
+
 	});
 
+
+	lsmApp.controller('campaignController', function($scope) {
+		$scope.message = 'Campaign us! JK. This is just a demo.';
+
+		$scope.createCampaign = function(){
+
+			$scope.message=$scope.campaignName;
+		};
+
+	});
